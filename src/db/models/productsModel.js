6d09102290d8cd67d6a85d30709ef7487a4303ca
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-12 13:58:34 -0500

    Add mongoose validations

--- src/db/models/productsModel.js
+++ src/db/models/productsModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const productsSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    currency: { type: String, required: true },
-    description: { type: String, required: true },
+    name: { type: String, required: true, minLength: 3, maxLength: 20 },
+    price: { type: Number, required: true, min: 10000 },
+    currency: { type: String, required: true, enum: ["COP", "USD"] },
+    description: { type: String, required: false, maxLength: 20 },
 });
 
 const ProductsModel = mongoose.model("products-information", productsSchema);
